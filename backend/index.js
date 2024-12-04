8b673c21cfdf92ab1a81cc7985668fa915bd2112
sam <manor@hey.com>
2024-12-04 12:25:13 -0500

    3.13

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,64 @@
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-const Person = require("./models/person");
-
 require("dotenv").config();
+mongoose.set("strictQuery", false);
+// const Person = require("./models/person");
+const Note = require("./models/note");
 
-// const MONGODB_URI = `mongodb+srv://samuelmmanor:${password}@cluster0.ysn4z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const url = process.env.MONGODB_URI;
+const http = require("http");
+const express = require("express");
+const app = express();
 
-console.log("connecting to", url);
+app.use(express.json());
 
-mongoose
-  .connect(url)
-  .then(() => {
-    console.log("connected to mongodb");
-  })
-  .catch((error) => {
-    console.log("error connecting to mongodb:", error.message);
+const cors = require("cors");
+app.use(cors());
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
+
+app.get("/api/notes", (req, res) => {
+  Note.find({}).then((notes) => {
+    res.json(notes);
   });
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  Note.findById(req.params.id).then((note) => {
+    res.json(note);
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+
+  if (body.content === undefined) {
+    return res.status(400).json({ error: "content missing" });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    res.json(savedNote);
+  });
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
